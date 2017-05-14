1bb231596c4c66a56c02c4654b2b8fe613e81074
Axel Prieto <prietoaxel@gmail.com>
2017-05-14 15:13:29 -0300

    Daily programming practice

--- EFIP1/Models/Calificacion.cs
+++ EFIP1/Models/Calificacion.cs
@@ -8,8 +8,6 @@ namespace EFIP1.Models
 
         public int NroOrdenCompra { get; set; }
 
-        //public DateTime FechaEntrega { get; set; }
-
         public FormaPago FormaPago { get; set; }
 
         public int Puntaje { get; set; }
